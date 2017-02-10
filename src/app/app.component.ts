b2721ed73e68718aedd705188e6bee2b3164e7ea
Alex Nye <alnye655321@gmail.com>
2017-02-09 21:22:00 -0700

    added tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { TabsPage } from '../pages/tabs/tabs';
-
+import { ListPage } from '../pages/list/list';
 
 @Component({
   templateUrl: 'app.html'
