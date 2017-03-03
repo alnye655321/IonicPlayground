6214263de0ed6536af492b3d8683076c86c83993
Alex Nye <alnye655321@gmail.com>
2017-03-02 20:47:17 -0700

    added a toast notification

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +8,13 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController,) {
+
+    let toast = this.toastCtrl.create({
+      message: 'Your account was successfully credited',
+      duration: 4000
+    });
+    toast.present();
 
   }
 
